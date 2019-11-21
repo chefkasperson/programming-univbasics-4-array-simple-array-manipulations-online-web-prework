6473c56ba6486f267deb682b4b5cd4819109ec67
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 04:24:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,8 +52,8 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
 
-def using_delete(instructors)
-  instructors = ["Josh", "Steven", "Sophie", "Amanda", "Steven"]
+def using_delete(instructors, "Steven")
+  instructors = ["Josh", "Steven", "Steven", "Sophie", "Amanda", "Steven"]
   no_offense_steven = instructor.delete("Steven")
 end
 
