17220e5fa689363dd547497017a16bec422e039f
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 03:34:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  deleted_string = using_pop(continents).pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = dog_breeds.pop(2)
+end
+
+
