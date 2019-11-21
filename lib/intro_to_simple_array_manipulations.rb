052d998dd5717aea1ef19e1fe7169d64a6071f5e
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 03:45:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,7 +30,11 @@ def shift_with_args(ice_cream_brands)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  all_my_favs = my_favorite_things.concat(more_favs)
+end
 
 
 
